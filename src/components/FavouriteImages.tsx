283023bbba964d98038c92cef8f462b3e8a9a50d
Sambit Sankalp <sambit.sankalp.official@gmail.com>
2023-01-07 00:44:01 +0530

    feat:data fetched and favourite feature done

---
+++ src/components/FavouriteImages.tsx
@@ -0,0 +1,79 @@
+import React, { FC } from 'react'
+import styled from '@emotion/styled'
+
+// Components
+import Heart from './icons/Icon'
+import Favourite from './icons/Favourite'
+
+export type Props = {
+  favourites: string[]
+  addFavourites: (
+    event: React.MouseEvent<HTMLImageElement, MouseEvent>,
+    dogImageLink: string,
+  ) => void
+}
+
+const FavouriteImages: FC<Props> = ({ favourites, addFavourites }) => {
+  return (
+    <Container>
+      <Header>
+        <Heart icon="redHeartIcon" alt="red heart icon" />
+        <Title>Favorites</Title>
+      </Header>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <ImageContainer>
+          {favourites &&
+            favourites.map((favourite) => (
+              <ImageDiv>
+                <img key={favourite} src={favourite} />
+                <Favourite
+                  onFavourite={(e) => addFavourites(e, favourite)}
+                  icon="redHeartIcon"
+                  alt="red heart icon"
+                />
+              </ImageDiv>
+            ))}
+        </ImageContainer>
+      </div>
+    </Container>
+  )
+}
+
+const Container = styled.div({
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'flex-start',
+  alignItems: 'flex-start',
+  textAlign: 'left',
+})
+
+const Header = styled.div({
+  display: 'flex',
+  justifyContent: 'flex-start',
+  alignItems: 'center',
+  marginLeft: '10px',
+})
+
+const Title = styled.h1({
+  fontWeight: 'bold',
+  fontSize: '24px',
+  lineHeight: '33px',
+  marginLeft: '10px',
+})
+
+const ImageContainer = styled.div({
+  marginTop: '20px',
+  display: 'grid',
+  gridTemplateColumns: 'auto auto auto auto',
+  gap: '30px',
+})
+
+const ImageDiv = styled.div({
+  position: 'relative',
+  width: '150px',
+  height: '150px',
+  borderRadius: '7px',
+})
+
+export default FavouriteImages
---
+++ src/components/icons/Favourite.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react'
+import styled from '@emotion/styled'
+
+// Assets
+import { icons } from '../../assets'
+
+interface Props {
+  icon: string
+  alt: string
+  onFavourite?: (event: React.MouseEvent<HTMLImageElement, MouseEvent>) => void
+}
+
+const Favourite: FC<Props> = ({ onFavourite, icon, alt }) => {
+  return <FavouriteIcon onClick={onFavourite} src={icons[icon]} alt={alt} />
+}
+
+const FavouriteIcon = styled.img({
+  position: 'absolute',
+  bottom: '10px',
+  right: '10px',
+  width: '17px',
+  height: '15px',
+})
+
+export default Favourite
